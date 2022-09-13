8a5fbd0e347767ad243719768e1ce4113398e65c
Vadim <zXcSven007@gmail.com>
2022-09-13 17:58:26 +0300

    Keypad added
    Added keypad functions on first fragment.

--- app/src/main/java/com/example/androidtest_1/FirstFragment.kt
+++ app/src/main/java/com/example/androidtest_1/FirstFragment.kt
@@ -1,12 +1,15 @@
 package com.example.androidtest_1
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.androidtest_1.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -19,8 +22,7 @@ class FirstFragment : Fragment() {
 
     private val binding get() = _binding!!
 
-    private val enteredNum: Int = -1
-    private val textField: String = ""
+    private var textField: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,12 +37,63 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        //binding.buttonFirst.setOnClickListener {
+        //    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        //}
+
+        binding.bNumC.setOnClickListener {
+            textField = ""
+            binding.displayField.text = textField
         }
 
         binding.bNum0.setOnClickListener {
-            textField + "0"
+            textField = textField.padEnd(textField.count() + 1, '0')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum1.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '1')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum2.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '2')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum3.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '3')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum4.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '4')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum5.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '5')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum6.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '6')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum7.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '7')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum8.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '8')
+            binding.displayField.text = textField
+        }
+
+        binding.bNum9.setOnClickListener {
+            textField = textField.padEnd(textField.count() + 1, '9')
+            binding.displayField.text = textField
         }
     }
 
